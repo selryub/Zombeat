c6a7b53387503faa99830b9ad2069da65722c39f
epaharya <effaharyanee17@gmail.com>
2025-06-11 03:46:05 +0800

    Change admin structure

--- admin/admin_financialRecord.php
+++ admin/admin_financialRecord.php
@@ -1,1 +1,18 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Profile</title>
+    <link rel="stylesheet" href="profile.css">
+    <link rel="stylesheet" href="adminstyle.css">
+</head>
+<body>
+<?php include "admin_frame.php"; ?>
+
+
+    <!--Link to JavaScript-->
+    <script src="admin.js"></script>
+
+
--- admin/admin_sales.php
+++ admin/admin_sales.php
@@ -1,1 +1,24 @@
+<?php
+session_start();
+
+if (isset($_SESSION["username"]) && $_SESSION["role"] !== "admin") {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="adminstyle.css">
+</head>
+<body>
+<?php include "admin_frame.php"; ?>
+
+
+    <!--Link to JavaScript-->
+    <script src="admin.js"></script>
+
 
