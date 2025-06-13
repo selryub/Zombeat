cb1c15afbfc214501ce192b06ca66455a4333a97
eizlyn <100139@siswa.unimas.my>
2025-06-13 13:52:34 +0700

    order_details

--- Employee/order_details.php
+++ Employee/order_details.php
@@ -1,9 +1,19 @@
+<?php
+session_start();
+
+if (isset($_SESSION["username"]) && $_SESSION["role"] !== "admin") {
+    header("Location: login.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Order Detail</title>
-  <link rel="stylesheet" href="../css/style.css"> <!-- Or your actual CSS path -->
+  <link rel="stylesheet" href="employeestyle.css"> 
   <style>
     body {
       font-family: Arial, sans-serif;
