9eb2b9703d27aa921d73690e803ab06a09acfd81
epaharya <effaharyanee17@gmail.com>
2025-06-19 01:32:30 +0800

    comment admin

--- admin/admin_employee.php
+++ admin/admin_employee.php
@@ -20,7 +20,7 @@ if ($result->num_rows > 0) {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Profile</title>
+    <title>Admin - Employee List</title>
     <link rel="stylesheet" href="employeelist.css">
 </head>
 <body>
--- admin/logout.php
+++ admin/logout.php
@@ -1,9 +1,8 @@
 <?php
-session_start();
-session_unset();
-session_destroy();
+session_start();  //must be called before using any session fx
+session_unset();  //unset all session variables (clears $_SESSION array)
+session_destroy(); //Destroy the session entirely (remove session data)
 
-
-
+//Redirect to the login page after logging out
 header("Location: ../PUBLIC/login.php");
 exit();
